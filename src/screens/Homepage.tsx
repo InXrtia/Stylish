f32a63df04419ab4331f05737b3acf5e7b4693f6
Shrey Parihar <88708702+InXrtia@users.noreply.github.com>
2024-06-20 03:58:59 +0530

    Your commit message

--- src/screens/Homepage.tsx
+++ src/screens/Homepage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, Image, Settings } from 'react-native';
-// import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import { Button } from 'react-native-paper';
 import Main from '../screens/Trending'
 import Search from '../screens/Search'
@@ -10,29 +9,33 @@ import Wishlist from '../screens/Wishlist'
 
 
 const Homepage: React.FC<{ navigation: any }> = ({ navigation }) => {
-  const [selectedTab, setelectedTab] = useState(0);
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <View style={{ flex: 1 }}>
-      {selectedTab==0?(<Main/>):selectedTab==1?(<Search/>):selectedTab==2?(<Cart/>):selectedTab==0?(<Wishlist/>):(<Profile/>)}
+      {selectedTab == 0 ? <Main /> : 
+       selectedTab == 1 ? <Search /> : 
+       selectedTab == 2 ? <Cart /> : 
+       selectedTab == 3 ? <Wishlist /> : 
+       <Profile />}
       <View style={styles.footer}>
-        <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Trending')}>
+        <TouchableOpacity style={styles.footerButton} onPress={() => setSelectedTab(0)}>
           <Image source={require('../images/home.png')} style={styles.icon} />
           <Text>Home</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton1}onPress={() => navigation.navigate('Search')}>
+        <TouchableOpacity style={styles.footerButton} onPress={() => setSelectedTab(1)}>
           <Image source={require('../images/Vector.png')} style={styles.icon} />
           <Text>Wishlist</Text>
         </TouchableOpacity>
         <View style={styles.cartButtonWrapper}>
-          <TouchableOpacity style={styles.cartButton}onPress={() => navigation.navigate('Cart')}>
+          <TouchableOpacity style={styles.cartButton} onPress={() => setSelectedTab(2)}>
             <Image source={require('../images/shoppingcart.png')} style={styles.icon} />
           </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.footerButton2}onPress={() => navigation.navigate('Wishlist')}>
+        <TouchableOpacity style={styles.footerButton} onPress={() => setSelectedTab(3)}>
           <Image source={require('../images/search.png')} style={styles.icon} />
           <Text>Search</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.footerButton3}onPress={() => navigation.navigate('Profile')}>
+        <TouchableOpacity style={styles.footerButton} onPress={() => setSelectedTab(4)}>
           <Image source={require('../images/setting.png')} style={styles.icon} />
           <Text>Settings</Text>
         </TouchableOpacity>
@@ -56,22 +59,6 @@ const styles = StyleSheet.create({
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-  },footerButton1: {
-    width: '20%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },footerButton2: {
-    width: '20%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  footerButton3: {
-    width: '20%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   icon: {
     width: 24,
@@ -91,7 +78,6 @@ const styles = StyleSheet.create({
     bottom: 10,
     borderRadius: 50,
     backgroundColor: '#FFFFFF',
-  
   },
 });
 
